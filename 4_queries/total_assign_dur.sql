5533cb60b90af68b147e47c68bcb3f438e6d85db
Justin Tu <coppolavina@gmail.com>
2021-11-26 19:43:26 +0000

    Set up the JavaScript project

--- 4_queries/total_assign_dur.sql
+++ 4_queries/total_assign_dur.sql
@@ -1,4 +1,4 @@
-SELECT day, count(*) as number_of_assignments, sum(duration)
+SELECT day, count(*) as number_of_assignments, sum(duration) as duration
 FROM assignments
 GROUP BY day
 ORDER BY assignments.day;
